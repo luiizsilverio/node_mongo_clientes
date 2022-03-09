85879703d340d4094d17719bf35d59c99680460d
luiizsilverio <luiiz.silverio@gmail.com>
2022-03-09 20:38:43 -0300

    CRUD completo

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -6,16 +6,34 @@ const { ObjectId } = require('mongodb')
 
 const routes = express.Router()
 
-routes.get('/', async(req, res) => {
-  try {
+routes.get('/find', async(req, res) => {
+  const { nome, idade, email } = req.query
+  let id
+  const obj = {}
+  if (req.query.id) {
+    id = new ObjectId(req.query.id)
+    obj._id = id
+  }
 
+  if (idade) {
+    obj.idade = Number(idade)
+  }
+
+  if (nome) {
+    obj.nome = nome.toString().trim()
+  }
+
+  if (email) {
+    obj.email = email.toString().trim()
+  }
+
+  try {
     const clientes = await db.getDb()
       .db()
       .collection('clientes')
-      .find()
+      .find(obj)
       .toArray()
 
-    console.log(clientes)
     res.send(clientes)
 
   } catch(error) {
@@ -23,6 +41,42 @@ routes.get('/', async(req, res) => {
     res.status(400).send({erro: error})
   }
 })
+
+
+routes.get('/:id', async(req, res) => {
+  const id = new ObjectId(req.params.id)
+
+  try {
+    const cliente = await db.getDb()
+      .db()
+      .collection('clientes')
+      .findOne({ _id: id })
+
+    res.send(cliente)
+
+  } catch(error) {
+    console.log({erro: error})
+    res.status(400).send({erro: error})
+  }
+})
+
+
+routes.get('/', async(req, res) => {
+  try {
+    const clientes = await db.getDb()
+      .db()
+      .collection('clientes')
+      .find()
+      .toArray()
+
+    res.send(clientes)
+
+  } catch(error) {
+    console.log({erro: error})
+    res.status(400).send({erro: error})
+  }
+})
+
 
 routes.post('/', (req, res) => {
   try {
@@ -41,7 +95,7 @@ routes.post('/', (req, res) => {
     const id = new ObjectId(req.params.id)
 
     const newCliente = {
-      id,
+      _id: id,
       nome,
       idade,
       email
@@ -54,6 +108,57 @@ routes.post('/', (req, res) => {
 
     res.status(301).send(newCliente)
     // res.redirect(301, '/')
+
+  } catch(error) {
+    console.log({erro: error})
+    res.status(400).send({erro: error})
+  }
+})
+
+
+routes.put('/:id', async(req, res) => {
+  const { nome, idade, email } = req.body
+  const id = new ObjectId(req.params.id)
+  const updCliente = {}
+
+  try {
+    if (idade) {
+      updCliente.idade = Number(idade)
+    }
+
+    if (nome) {
+      updCliente.nome = nome.toString().trim()
+    }
+
+    if (email) {
+      updCliente.email = email.toString().trim()
+    }
+
+    const newCliente = await db.getDb()
+      .db()
+      .collection('clientes')
+      .updateOne({ _id: id }, {$set: updCliente});
+
+    res.send(newCliente)
+    // res.redirect(301, '/')
+
+  } catch(error) {
+    console.log({erro: error})
+    res.status(400).send({erro: error})
+  }
+})
+
+
+routes.delete('/:id', async(req, res) => {
+  const id = new ObjectId(req.params.id)
+
+  try {
+    const cliente = await db.getDb()
+      .db()
+      .collection('clientes')
+      .deleteOne({ _id: id })
+
+    res.send(cliente)
 
   } catch(error) {
     console.log({erro: error})
